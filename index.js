6055c0ece9f897cf82d688306d12a23056e71178
Lassi Palojärvi <lassi.palojarvi@outlook.com>
2019-08-16 14:42:12 +0300

    3.4

--- index.js
+++ index.js
@@ -41,6 +41,14 @@ app.get("/api/persons/:id", (req, res) => {
         res.status(404).end()
 })
 
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
 const PORT = 3001
 
 app.listen(PORT)
