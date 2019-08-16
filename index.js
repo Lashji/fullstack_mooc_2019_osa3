8ee9396b1ab27568089630b375c7b53184de1599
Lassi Palojärvi <lassi.palojarvi@outlook.com>
2019-08-16 14:25:36 +0300

    3.2

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ app.get("/api/persons", (req, res) => {
     res.json(persons)
 })
 
-
+app.get("/info", (req, res) => {
+    res.send(`Phonebook has info for ${persons.length} people\n${new Date()}`)
+})
 
 
 const PORT = 3001
